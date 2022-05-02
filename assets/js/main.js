c82cad3f2193ef441f0f939385582f813248f771
Romain Vasseur <romain.vasseur@ansys.com>
2022-05-02 13:50:46 +0200

    Fix ToC

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,15 +15,15 @@ var sectionHeight = function() {
   $(function() {
     // Build dynamic ToC section
     $("section h2, section h3").each(function(){
-      $("toc ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
+      $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
       $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
-      $("toc ul li:first-child a").parent().addClass("active");
+      $("nav ul li:first-child a").parent().addClass("active");
     });
   
-    $("toc ul li").on("click", "a", function(event) {
+    $("nav ul li").on("click", "a", function(event) {
       var position = $($(this).attr("href")).offset().top - 190;
       $("html, body").animate({scrollTop: position}, 400);
-      $("toc ul li a").parent().removeClass("active");
+      $("nav ul li a").parent().removeClass("active");
       $(this).parent().addClass("active");
       event.preventDefault();
     });
